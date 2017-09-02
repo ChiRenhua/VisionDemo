9303f3b78f943135808f406cdbac10654000d22c
Martin <crh944015688@gmail.com>
2017-09-02 17:33:48 +0800

    Initial Commit

---
+++ VisionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VisionDemo
+//
+//  Created by 迟人华 on 2017/9/2.
+//  Copyright © 2017年 迟人华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
